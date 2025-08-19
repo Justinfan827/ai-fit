6d946ca0952ebe21650d2099a2e5b777f37dace3
Justin Fan <justinfan827@gmail.com>
2025-08-18 23:52:05 -0700

    fix old supabase call

--- src/app/home/(sidebar)/programs/new-program-button-with-data.tsx
+++ src/app/home/(sidebar)/programs/new-program-button-with-data.tsx
@@ -1,24 +1,11 @@
-import { createServerClient } from "@/lib/supabase/create-server-client"
-import { getAllExercises } from "@/lib/supabase/server/users/trainer-repo"
+import { getCachedAuthUserT } from "@/lib/supabase/server/auth-utils"
+import { getCachedAllExercisesT } from "@/lib/supabase/server/users/trainer-repo"
 import NewProgramButton from "./new-program-button"
 
 export default async function NewProgramButtonWithData() {
-  const serverClient = await createServerClient()
-  const { data: sessiondata, error } = await serverClient.auth.getSession()
-
-  if (error) {
-    return <div>Error: {error.message}</div>
-  }
-
-  if (sessiondata.session === null) {
-    return <div>Error: no session</div>
-  }
-
-  const exercises = await getAllExercises(sessiondata.session.user.id)
-
-  if (exercises.error) {
-    return <div>Error: {exercises.error.message}</div>
-  }
-
-  return <NewProgramButton exercises={exercises.data.base} />
+  const authUser = await getCachedAuthUserT()
+  const exercises = await getCachedAllExercisesT(authUser.userId)
+  return (
+    <NewProgramButton exercises={exercises.base.concat(exercises.custom)} />
+  )
 }
